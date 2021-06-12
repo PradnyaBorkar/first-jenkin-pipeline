d907b3f25f3effd334093ccd41788b5a8d1b2f97
PradnyaBorkar <pradnya.borkar@springernature.com>
2021-06-12 19:43:26 +0530

    add python script to check jenkin job status

--- jenkins_job_status.py
+++ jenkins_job_status.py
@@ -1,15 +1,17 @@
 import json
 import requests
 import time
+from requests.auth import HTTPBasicAuth
 
 job_name = "first-pipeline"
 
 
 def jenkins_job_status():
     try:
-        url = "http://localhost:8080/job/"+job_name+"/lastBuild/api/json"
+        url = "http://localhost:8080/job/" + job_name + "/lastBuild/api/json"
+        google_url = "https://www.google.com/search?q=hello"
         while True:
-            data = requests.get(url).json()
+            data = requests.get(url, auth=HTTPBasicAuth('Pradnya', 'Pradnya')).json()
             if data['building']:
                 time.sleep()
             else:
